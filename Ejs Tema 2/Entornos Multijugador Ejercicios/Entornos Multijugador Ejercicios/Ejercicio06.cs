c99e93d0c88250a2e15d87e2673955d9c8d5653a
Mdoc14 <marcos.deozaeta@gmail.com>
2024-02-08 19:51:38 +0100

    Ejercicio 6. Museo con muchas visitas
    Uso del mismo mutex en varias partes del codigo con la misma variable compartida

--- Ejs Tema 2/Entornos Multijugador Ejercicios/Entornos Multijugador Ejercicios/Ejercicio06.cs
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Entornos Multijugador Ejercicios/Ejercicio06.cs
@@ -1,13 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Entornos_Multijugador_Ejercicios
 {
     internal class Ejercicio06
     {
+        const int NUM_PERSONAS = 10;
+        const int NUM_ITERACIONES = 10;
+        static volatile int visitantesActuales = 0;
+        static Mutex cambioVisitantes = new Mutex();
+        static void Persona()
+        {
+            for (int i = 0; i < NUM_ITERACIONES; i++)
+            {
+                
 
+                cambioVisitantes.WaitOne();
+                visitantesActuales++;
+                WriteLine("Hola, somos " + visitantesActuales.ToString());
+                cambioVisitantes.ReleaseMutex();
+
+                Museo();
+
+                cambioVisitantes.WaitOne();
+                visitantesActuales--;
+                WriteLine("Adios a los " + visitantesActuales.ToString());
+                cambioVisitantes.ReleaseMutex();
+
+                for (int j = 0; j < NUM_ITERACIONES; j++)
+                {
+                    WriteLine("paseando ...");
+                }
+            }
+        }
+
+        static void Museo()
+        {
+            WriteLine("Hola");
+            WriteLine("Que bonito");
+            WriteLine("Alucinante");
+            WriteLine("Adios");
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < NUM_PERSONAS; i++)
+            {
+                Thread persona = new Thread(Persona);
+                persona.Name = new string('\t', i) + "Persona " + i;
+                persona.Start();
+            }
+
+        }
+
+        static void WriteLine(String s)
+        {
+            Thread.Sleep(10);
+            Console.WriteLine(Thread.CurrentThread.Name + ": " + s);
+            Thread.Sleep(10);
+        }
     }
 }
