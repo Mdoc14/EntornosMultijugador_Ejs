b56be6b7c695be169d1d43b9583ea63b3d6f2b4f
Marcos De Ozaeta Cabadas <marcos.deozaeta@gmail.com>
2024-02-16 14:43:06 +0100

    Ejercicios 14 y 15.  Print letras

---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Entornos Multijugador Ejercicios/Ejercicio14.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Entornos_Multijugador_Ejercicios
+{
+    internal class Ejercicio14
+    {
+        const int NHILOS = 40;
+        static volatile  int hilosCompletados = 0;
+        static SemaphoreSlim emHilosCompletados = new SemaphoreSlim(1);
+        static SemaphoreSlim scSiguienteLetra = new SemaphoreSlim(0);
+
+
+        public static void printLetras() 
+        {
+            Write("A");
+
+            emHilosCompletados.Wait();
+            hilosCompletados++;
+            if(hilosCompletados == NHILOS) 
+            {
+                scSiguienteLetra.Release(NHILOS);
+            }
+            emHilosCompletados.Release();
+            
+            scSiguienteLetra.Wait();
+            Write("B");
+
+        }
+        static void Main() 
+        {
+           
+            for(int i = 0; i < NHILOS; ++i) 
+            {
+                new Thread(printLetras).Start();
+            }
+        
+        }
+        static void Write(String s)
+        {
+            Thread.Sleep(10);
+            Console.Write(s);
+            Thread.Sleep(10);
+        }
+
+    }
+}
---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Entornos Multijugador Ejercicios/Ejercicio15.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace Entornos_Multijugador_Ejercicios
+{
+    internal class Ejercicio15
+    {
+        const int NHILOS = 4;
+        static volatile int hilosCompletados = 0;
+
+        static SemaphoreSlim emHilosCompletados = new SemaphoreSlim(1);
+        static SemaphoreSlim scSiguienteLetra = new SemaphoreSlim(0);
+        static SemaphoreSlim scPararUltimo = new SemaphoreSlim(0);
+        static SemaphoreSlim scPararOtros = new SemaphoreSlim(0);
+
+
+        public static void printLetras(object o)
+        {
+            char letra = (char)o;
+
+            while (true) 
+            {
+                Write(letra);
+
+                emHilosCompletados.Wait();
+                hilosCompletados++;
+
+                if (hilosCompletados < NHILOS)
+                {
+                    emHilosCompletados.Release();
+                    if(letra != 'A') Thread.Sleep(1000);
+                    scSiguienteLetra.Wait();
+                    scPararUltimo.Release();
+                    scPararOtros.Wait();
+                }
+                else 
+                {
+                    emHilosCompletados.Release();
+                    Write('-');
+                    hilosCompletados = 0;
+                    scSiguienteLetra.Release(NHILOS - 1);
+                    for (int i = 0; i < NHILOS - 1; i++)
+                    {
+                        scPararUltimo.Wait();
+                    }
+                    scPararOtros.Release(NHILOS - 1);
+                }
+
+            }
+        }
+        static void Main()
+        {
+            char c = 'A';
+            for (int i = 0; i < 4; ++i)
+            {
+                new Thread(printLetras).Start(c++);
+            }
+
+        }
+        static void Write(char s)
+        {
+            Thread.Sleep(10);
+            Console.Write(s);
+            Thread.Sleep(10);
+        }
+    }
+}
