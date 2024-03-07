417cea05534e454aa96f093fe7a3c4e36ab7a973
Mdoc14 <marcos.deozaeta@gmail.com>
2024-03-07 21:24:00 +0100

    Tema 4. Ejercicio 1 e intento de ejercicio 2(no se si está bien)

---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Tema 4/Ejercicio01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Tema_4
+{
+    internal class Ejercicio01
+    {
+        SemaphoreSlim sem = new SemaphoreSlim(0);
+        volatile int producto;
+
+        void Productor()
+        {
+            Random random = new Random();
+            producto = random.Next(10);
+            sem.Release();    
+        }
+
+        void Consumidor()
+        {
+            sem.Wait();
+            Console.WriteLine("Producto: {0}", producto);
+        }
+
+        void Exec()
+        {
+            new Thread(Productor).Start();
+            new Thread(Consumidor).Start();
+        }
+
+        static void Main(string[] args)
+        {
+            new Ejercicio01().Exec();
+        }
+    }
+}
---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Tema 4/Ejercicio02.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace Tema_4
+{
+    internal class Ejercicio02
+    {
+        void Mensaje()
+        {
+            try
+            {
+                Console.WriteLine("[Thread Mensaje] La vida es bella");
+                Thread.Sleep(2000);
+                Console.WriteLine("[Thread Mensaje] O no...");
+                Thread.Sleep(2000);
+                Console.WriteLine("[Thread Mensaje] Los pajaritos cantan");
+                Thread.Sleep(2000);
+                Console.WriteLine("[Thread Mensaje] Y molestan...");
+            }
+            catch(ThreadInterruptedException e)
+            {
+                Console.WriteLine("[Thread Mensaje] Se acabó!");
+            }
+
+        }
+        void Exec() 
+        {
+            Thread mensaje = new Thread(Mensaje);
+            mensaje.Start();
+
+            for (int i = 0; i < 5; i++) 
+            {
+                if (!mensaje.IsAlive) 
+                {
+                    break;
+                }
+                Thread.Sleep(1000);
+                Console.WriteLine("[Thread Main] Todavia esperando...");
+            }
+            if (mensaje.IsAlive)
+            {
+                mensaje.Interrupt();
+                Console.WriteLine("[Thread Main] Cansado de esperar");
+            }
+            
+            mensaje.Join();
+
+            Console.WriteLine("[Thread Main] Por fin!!");
+
+
+        }
+        static void Main() 
+        { 
+            new Ejercicio02().Exec();
+        }
+    }
+}
