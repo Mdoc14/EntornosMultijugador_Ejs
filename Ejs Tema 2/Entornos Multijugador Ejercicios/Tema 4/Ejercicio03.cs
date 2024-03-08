a0c292b7b1fb143586c8b7fa15adc2c309527299
Mdoc14 <marcos.deozaeta@gmail.com>
2024-03-08 16:26:37 +0100

    Ejercicios 3 y 4. Tema 4. Clases threadsafe

---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Tema 4/Ejercicio03.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace Tema_4
+{
+    internal class Ejercicio03
+    {
+        static void Main() 
+        {
+            new ProdCons().Exec();
+        }
+    }
+    class ProdCons
+    {
+        volatile float producto;
+        SincCon sinc = new SincCon();
+        public void Productor()
+        {
+            Random random = new Random();
+            producto = random.Next(1, 10);
+            sinc.Signal();
+        }
+        public void Consumidor()
+        {
+            sinc.Await();
+            Console.WriteLine("Producto: {0}", producto);
+        }
+        public void Exec()
+        {
+            new Thread(() => Productor()).Start();
+            new Thread(() => Consumidor()).Start();
+        }
+    }
+
+    class SincCon
+    {
+        volatile bool producido = false;
+        public void Signal()
+        {
+            producido = true;
+        }
+
+        public void Await()
+        {
+            while (!producido);
+            producido = false;
+        }
+    }
+}
---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Tema 4/Ejercicio04.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace Tema_4
+{
+    internal class Ejercicio04
+    {
+        static void Main()
+        {
+            new ProdConsN().Exec();
+        }
+    }
+    class ProdConsN
+    {
+        volatile float producto;
+        SincConN producido = new SincConN(false);
+        SincConN consumido = new SincConN(true);
+        public void Productor()
+        {
+            Random random = new Random();
+            int cache;
+            while (true)
+            {
+                cache = random.Next(1, 10);
+                consumido.Await();
+                producto = cache;
+                Console.WriteLine("[PROD] Producto: {0}", producto);
+                producido.Signal();
+            }
+        }
+        public void Consumidor()
+        {
+            while (true)
+            {
+                producido.Await();
+                Console.WriteLine("[CONS] Producto: {0}", producto);
+                Console.ReadLine();
+                consumido.Signal();
+            }
+        }
+        public void Exec()
+        {
+            new Thread(() => Productor()).Start();
+            new Thread(() => Consumidor()).Start();
+        }
+    }
+
+    class SincConN
+    {
+        volatile bool producido;
+
+        public SincConN(bool producido)
+        {
+            this.producido = producido;
+        }
+        public void Signal()
+        {
+            producido = true;
+        }
+
+        public void Await()
+        {
+            while (!producido) ;
+            producido = false;
+        }
+    }
+}
