aab78bccedabd7671a34e67bf07cabbdaede4f28
Mdoc14 <marcos.deozaeta@gmail.com>
2024-03-15 23:00:06 +0100

    Ejercicios 6 y 8. Lock y barrier

---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Tema 4/Ejercicio06.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Tema_4
+{
+    internal class Ejercicio06
+    {
+        const int N_PERSONAS = 5;
+        int nPersonasMuseo = 0;
+        readonly object emPersonasMuseo = new object();
+        void Persona()
+        {
+            bool regalo = false;
+            lock (emPersonasMuseo)
+            {
+                nPersonasMuseo++;
+                regalo = nPersonasMuseo == 1 ? true : false;
+            }
+            if (regalo) Console.Write("Tengo regalo");
+
+            Console.WriteLine("Que bonito");
+            Console.WriteLine("Alucinante");
+
+            lock (emPersonasMuseo)
+            {
+                nPersonasMuseo--;
+            }
+        }
+        void Exec()
+        { 
+            for (int i = 0; i < N_PERSONAS; i++)
+            {
+                new Thread(Persona).Start();
+            }
+        
+        }
+
+        static void Main()
+        {
+            new Ejercicio06().Exec();
+        }
+    }
+
+}
---
+++ Ejs Tema 2/Entornos Multijugador Ejercicios/Tema 4/Ejercicio08.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Tema_4
+{
+    internal class Ejercicio08
+    {
+        const int N_PROCESOS = 5;
+        Barrier barrera;
+        void Proceso()
+        {
+            while (true)
+            {
+                Console.Write('A');
+                barrera.SignalAndWait();
+                Console.Write('B');
+                barrera.SignalAndWait();
+            }
+        }
+
+        void BarreraProcesos(Barrier b)
+        {
+            Console.Write(b.CurrentPhaseNumber % 2 == 0 ? '-' : '\n');
+        }
+
+        void Exec()
+        {
+            barrera = new Barrier(N_PROCESOS, BarreraProcesos);
+            for (int i = 0; i < N_PROCESOS; i++)
+            {
+                new Thread(Proceso).Start();
+            }
+        }
+        static void Main()
+        {
+            new Ejercicio08().Exec();
+        }
+    }
+}
